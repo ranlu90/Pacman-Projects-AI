370ed21f0b539f0259043f39d369a9bca0ab4824
Ran Lu <ranlu90@hotmail.com>
2018-05-08 23:53:21 +1000

    RL agent dependencies require initial parameters

--- pacman-contest/myTeam.py
+++ pacman-contest/myTeam.py
@@ -18,12 +18,13 @@ from game import Directions
 import game
 from util import nearestPoint, Counter
 
+
 #################
 # Team creation #
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first='DummyAgent', second='DummyAgent'):
+               first='ApproximateQAgent', second='ApproximateQAgent'):
     """
     This function should return a list of two agents that will form the
     team, initialized using firstIndex and secondIndex as their agent
@@ -46,96 +47,6 @@ def createTeam(firstIndex, secondIndex, isRed,
 ##########
 # Agents #
 ##########
-
-class QLearningAgent(CaptureAgent):
-    """
-    A Dummy agent to serve as an example of the necessary agent structure.
-    You should look at baselineTeam.py for more details about how to
-    create an agent as this is the bare minimum.
-    """
-
-    def registerInitialState(self, gameState):
-        """
-        This method handles the initial setup of the
-        agent to populate useful fields (such as what team
-        we're on).
-
-        A distanceCalculator instance caches the maze distances
-        between each pair of positions, so your agents can use:
-        self.distancer.getDistance(p1, p2)
-
-        IMPORTANT: This method may run for at most 15 seconds.
-        """
-
-        '''
-        Make sure you do not delete the following line. If you would like to
-        use Manhattan distances instead of maze distances in order to save
-        on initialization time, please take a look at
-        CaptureAgent.registerInitialState in captureAgents.py.
-        '''
-        CaptureAgent.registerInitialState(self, gameState)
-
-        '''
-        Your initialization code goes here, if you need any.
-        '''
-
-        self.weights = util.Counter()
-
-
-    def chooseAction(self, gameState):
-        """
-        Picks among actions randomly.
-        """
-        actions = gameState.getLegalActions(self.index)
-
-        '''
-        You should change this in your own agent.
-        '''
-
-        return random.choice(actions)
-
-    def getWeights(self):
-        return self.weights
-
-    def getQValue(self, state, action):
-        """
-          Should return Q(state,action) = w * featureVector
-          where * is the dotProduct operator
-        """
-        features = self.getFeatures(state, action)
-        return Counter(self.weights) * Counter(features)
-
-    def getFeatures(self, state, action):
-
-        features = util.Counter()
-
-        features['food'] = len(self.getFood(state).asList())
-        features['defending'] = len(self.getFoodYouAreDefending(state).asList())
-        features['score'] = self.getScore(state)
-        # self.getCapsules(state)
-        # self.getOpponents(state)
-        # self.getCurrentObservation()
-
-        return features
-
-    def update(self, state, action, nextState, reward):
-        """
-           Should update your weights based on transition
-        """
-        features = self.featExtractor.getFeatures(state, action)
-        difference = (reward + self.discount * self.computeValueFromQValues(nextState)) - self.getQValue(state, action)
-        self.weights = {k: self.weights.get(k, 0) + self.alpha * difference * features.get(k, 0) for k in
-                        set(self.weights) | set(features)}
-
-    def final(self, state):
-        "Called at the end of each game."
-        # call the super-class final method
-        CaptureAgent.final(self, state)
-
-        # did we finish training?
-        if self.episodesSoFar == self.numTraining:
-            # you might want to print your weights here for debugging
-            print(self.weights)
 
 
 class DummyAgent(CaptureAgent):
@@ -170,7 +81,6 @@ class DummyAgent(CaptureAgent):
         Your initialization code goes here, if you need any.
         '''
 
-
     def chooseAction(self, gameState):
         """
         Picks among actions randomly.
@@ -184,35 +94,140 @@ class DummyAgent(CaptureAgent):
         return random.choice(actions)
 
 
-class ReflexCaptureAgent(CaptureAgent):
+class ApproximateQAgent(CaptureAgent):
     """
-    A base class for reflex agents that chooses score-maximizing actions
+    A Dummy agent to serve as an example of the necessary agent structure.
+    You should look at baselineTeam.py for more details about how to
+    create an agent as this is the bare minimum.
     """
+
+    def __init__(self, index):
+        CaptureAgent.__init__(self, index)
+        self.weights = util.Counter()
 
     def registerInitialState(self, gameState):
-        self.start = gameState.getAgentPosition(self.index)
+        """
+        This method handles the initial setup of the
+        agent to populate useful fields (such as what team
+        we're on).
+
+        A distanceCalculator instance caches the maze distances
+        between each pair of positions, so your agents can use:
+        self.distancer.getDistance(p1, p2)
+
+        IMPORTANT: This method may run for at most 15 seconds.
+        """
+
+        '''
+        Make sure you do not delete the following line. If you would like to
+        use Manhattan distances instead of maze distances in order to save
+        on initialization time, please take a look at
+        CaptureAgent.registerInitialState in captureAgents.py.
+        '''
         CaptureAgent.registerInitialState(self, gameState)
 
+        '''
+        Your initialization code goes here, if you need any.
+        '''
+
+    def computeActionFromQValues(self, state):
+        """
+          Compute the best action to take in a state.  Note that if there
+          are no legal actions, which is the case at the terminal state,
+          you should return None.
+        """
+        legal_actions = state.getLegalActions(self.index)
+        successor_q_values = {action: self.getQValue(state, action) for action in legal_actions}
+        max_action = max(successor_q_values.iterkeys(), key=(lambda key: successor_q_values[key]))
+        return max_action if legal_actions else None
+
+    def computeValueFromQValues(self, gameState):
+        """
+          Returns max_action Q(state,action)
+          where the max is over legal actions.  Note that if
+          there are no legal actions, which is the case at the
+          terminal state, you should return a value of 0.0.
+        """
+        legal_actions = gameState.getLegalActions(self.index)
+        successor_q_values = [self.getQValue(gameState, action) for action in legal_actions]
+        return max(successor_q_values) if legal_actions else 0.0
+
+    def getAction(self, gameState):
+        """
+          Compute the action to take in the current state.  With
+          probability self.epsilon, we should take a random action and
+          take the best policy action otherwise.  Note that if there are
+          no legal actions, which is the case at the terminal state, you
+          should choose None as the action.
+
+          HINT: You might want to use util.flipCoin(prob)
+          HINT: To pick randomly from a list, use random.choice(list)
+        """
+        # Pick Action
+        legalActions = gameState.getLegalActions(self.index)
+        random_action = util.flipCoin(self.epsilon)
+        action = self.computeActionFromQValues(gameState)
+        return random.choice(legalActions) if random_action else action
+
+    def doAction(self, state, action):
+        """
+            Called by inherited class when
+            an action is taken in a state
+        """
+        self.lastState = state
+        self.lastAction = action
+
     def chooseAction(self, gameState):
-        return 0
-
-    def getSuccessor(self, gameState, action):
         """
-        Finds the next successor which is a grid position (location tuple).
+        Picks among actions randomly.
         """
-        successor = gameState.generateSuccessor(self.index, action)
-        pos = successor.getAgentState(self.index).getPosition()
-        if pos != nearestPoint(pos):
-            # Only half a grid position was covered
-            return successor.generateSuccessor(self.index, action)
-        else:
-            return successor
+        actions = gameState.getLegalActions(self.index)
 
-    def evaluate(self, gameState, action):
-        return 0
+        '''
+        You should change this in your own agent.
+        '''
+        action = self.getAction(gameState)
+        self.doAction(gameState, action)
+        return action
 
-    def getFeatures(self, gameState, action):
-        return 0
+    def getWeights(self):
+        return self.weights
 
-    def getWeights(self, gameState, action):
-        return 0
+    def getQValue(self, gameState, action):
+        """
+          Should return Q(state,action) = w * featureVector
+          where * is the dotProduct operator
+        """
+        features = self.getFeatures(gameState, action)
+        return Counter(self.weights) * Counter(features)
+
+    def getFeatures(self, state, action):
+        features = util.Counter()
+
+        features['food'] = len(self.getFood(state).asList())
+        features['defending'] = len(self.getFoodYouAreDefending(state).asList())
+        features['score'] = self.getScore(state)
+        # self.getCapsules(state)
+        # self.getOpponents(state)
+        # self.getCurrentObservation()
+        return features
+
+    def update(self, gameState, action, nextState, reward):
+        """
+           Should update your weights based on transition
+        """
+        features = self.getFeatures(gameState, action)
+        difference = (reward + self.discount * self.computeValueFromQValues(nextState)) - self.getQValue(gameState,
+                                                                                                         action)
+        self.weights = {k: self.weights.get(k, 0) + self.alpha * difference * features.get(k, 0) for k in
+                        set(self.weights) | set(features)}
+
+    def final(self, state):
+        "Called at the end of each game."
+        # call the super-class final method
+        CaptureAgent.final(self, state)
+
+        # did we finish training?
+        if self.episodesSoFar == self.numTraining:
+            # you might want to print your weights here for debugging
+            print(self.weights)
