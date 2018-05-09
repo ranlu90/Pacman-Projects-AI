a4c87174bea6e6f255519ddaa67b714345715bb1
Ran Lu <ranlu90@hotmail.com>
2018-05-09 11:30:00 +1000

    fix all dependencies related to reinforcement learning

--- pacman-contest/myTeam.py
+++ pacman-contest/myTeam.py
@@ -95,13 +95,8 @@ class DummyAgent(CaptureAgent):
 
 
 class ApproximateQAgent(CaptureAgent):
-    """
-    A Dummy agent to serve as an example of the necessary agent structure.
-    You should look at baselineTeam.py for more details about how to
-    create an agent as this is the bare minimum.
-    """
 
-    def __init__(self, index, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=100, **args):
+    def __init__(self, index, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=0, **args):
         CaptureAgent.__init__(self, index)
         args['epsilon'] = epsilon
         args['gamma'] = gamma
@@ -116,64 +111,46 @@ class ApproximateQAgent(CaptureAgent):
         self.discount = float(gamma)
         self.weights = util.Counter()
 
+    def startEpisode(self):
+        self.lastState = None
+        self.lastAction = None
+        self.episodeRewards = 0.0
+
+    def stopEpisode(self):
+        if self.episodesSoFar < self.numTraining:
+            self.accumTrainRewards += self.episodeRewards
+        else:
+            self.accumTestRewards += self.episodeRewards
+        self.episodesSoFar += 1
+        if self.episodesSoFar >= self.numTraining:
+            # Take off the training wheels
+            self.epsilon = 0.0  # no exploration
+            self.alpha = 0.0  # no learning
+
+    def isInTraining(self):
+        return self.episodesSoFar < self.numTraining
+
+    def isInTesting(self):
+        return not self.isInTraining()
+
     def registerInitialState(self, gameState):
-        """
-        This method handles the initial setup of the
-        agent to populate useful fields (such as what team
-        we're on).
-
-        A distanceCalculator instance caches the maze distances
-        between each pair of positions, so your agents can use:
-        self.distancer.getDistance(p1, p2)
-
-        IMPORTANT: This method may run for at most 15 seconds.
-        """
-
-        '''
-        Make sure you do not delete the following line. If you would like to
-        use Manhattan distances instead of maze distances in order to save
-        on initialization time, please take a look at
-        CaptureAgent.registerInitialState in captureAgents.py.
-        '''
         CaptureAgent.registerInitialState(self, gameState)
-
-        '''
-        Your initialization code goes here, if you need any.
-        '''
+        self.startEpisode()
+        if self.episodesSoFar == 0:
+            print 'Beginning %d episodes of Training' % (self.numTraining)
 
     def computeActionFromQValues(self, state):
-        """
-          Compute the best action to take in a state.  Note that if there
-          are no legal actions, which is the case at the terminal state,
-          you should return None.
-        """
         legal_actions = state.getLegalActions(self.index)
         successor_q_values = {action: self.getQValue(state, action) for action in legal_actions}
         max_action = max(successor_q_values.iterkeys(), key=(lambda key: successor_q_values[key]))
         return max_action if legal_actions else None
 
     def computeValueFromQValues(self, gameState):
-        """
-          Returns max_action Q(state,action)
-          where the max is over legal actions.  Note that if
-          there are no legal actions, which is the case at the
-          terminal state, you should return a value of 0.0.
-        """
         legal_actions = gameState.getLegalActions(self.index)
         successor_q_values = [self.getQValue(gameState, action) for action in legal_actions]
         return max(successor_q_values) if legal_actions else 0.0
 
     def getAction(self, gameState):
-        """
-          Compute the action to take in the current state.  With
-          probability self.epsilon, we should take a random action and
-          take the best policy action otherwise.  Note that if there are
-          no legal actions, which is the case at the terminal state, you
-          should choose None as the action.
-
-          HINT: You might want to use util.flipCoin(prob)
-          HINT: To pick randomly from a list, use random.choice(list)
-        """
         # Pick Action
         legalActions = gameState.getLegalActions(self.index)
         random_action = util.flipCoin(self.epsilon)
@@ -181,22 +158,10 @@ class ApproximateQAgent(CaptureAgent):
         return random.choice(legalActions) if random_action else action
 
     def doAction(self, state, action):
-        """
-            Called by inherited class when
-            an action is taken in a state
-        """
         self.lastState = state
         self.lastAction = action
 
     def chooseAction(self, gameState):
-        """
-        Picks among actions randomly.
-        """
-        actions = gameState.getLegalActions(self.index)
-
-        '''
-        You should change this in your own agent.
-        '''
         action = self.getAction(gameState)
         self.doAction(gameState, action)
         return action
@@ -205,10 +170,6 @@ class ApproximateQAgent(CaptureAgent):
         return self.weights
 
     def getQValue(self, gameState, action):
-        """
-          Should return Q(state,action) = w * featureVector
-          where * is the dotProduct operator
-        """
         features = self.getFeatures(gameState, action)
         return Counter(self.weights) * Counter(features)
 
@@ -224,21 +185,60 @@ class ApproximateQAgent(CaptureAgent):
         return features
 
     def update(self, gameState, action, nextState, reward):
-        """
-           Should update your weights based on transition
-        """
         features = self.getFeatures(gameState, action)
         difference = (reward + self.discount * self.computeValueFromQValues(nextState)) - self.getQValue(gameState,
                                                                                                          action)
         self.weights = {k: self.weights.get(k, 0) + self.alpha * difference * features.get(k, 0) for k in
                         set(self.weights) | set(features)}
 
-    def final(self, state):
-        "Called at the end of each game."
-        # call the super-class final method
-        CaptureAgent.final(self, state)
+    def observeTransition(self, state, action, nextState, deltaReward):
+        self.episodeRewards += deltaReward
+        self.update(state, action, nextState, deltaReward)
 
-        # did we finish training?
+    def observationFunction(self, state):
+        if not self.lastState is None:
+            reward = state.getScore() - self.lastState.getScore()
+            self.observeTransition(self.lastState, self.lastAction, state, reward)
+        return state
+
+    def terminal(self, state):
+        deltaReward = state.getScore() - self.lastState.getScore()
+        self.observeTransition(self.lastState, self.lastAction, state, deltaReward)
+        self.stopEpisode()
+
+        # Make sure we have this var
+        if not 'episodeStartTime' in self.__dict__:
+            self.episodeStartTime = time.time()
+        if not 'lastWindowAccumRewards' in self.__dict__:
+            self.lastWindowAccumRewards = 0.0
+        self.lastWindowAccumRewards += state.getScore()
+
+        NUM_EPS_UPDATE = 100
+        if self.episodesSoFar % NUM_EPS_UPDATE == 0:
+            print 'Reinforcement Learning Status:'
+            windowAvg = self.lastWindowAccumRewards / float(NUM_EPS_UPDATE)
+            if self.episodesSoFar <= self.numTraining:
+                trainAvg = self.accumTrainRewards / float(self.episodesSoFar)
+                print '\tCompleted %d out of %d training episodes' % (
+                    self.episodesSoFar, self.numTraining)
+                print '\tAverage Rewards over all training: %.2f' % (
+                    trainAvg)
+            else:
+                testAvg = float(self.accumTestRewards) / (self.episodesSoFar - self.numTraining)
+                print '\tCompleted %d test episodes' % (self.episodesSoFar - self.numTraining)
+                print '\tAverage Rewards over testing: %.2f' % testAvg
+            print '\tAverage Rewards for last %d episodes: %.2f' % (
+                NUM_EPS_UPDATE, windowAvg)
+            print '\tEpisode took %.2f seconds' % (time.time() - self.episodeStartTime)
+            self.lastWindowAccumRewards = 0.0
+            self.episodeStartTime = time.time()
+
         if self.episodesSoFar == self.numTraining:
-            # you might want to print your weights here for debugging
+            msg = 'Training Done (turning off epsilon and alpha)'
+            print '%s\n%s' % (msg, '-' * len(msg))
+
+    def final(self, state):
+        # call the super-class final method
+        self.terminal(state)
+        if self.episodesSoFar == self.numTraining:
             print(self.weights)
